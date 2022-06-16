9435302ef7529fe529fc4007c8261a1e4f4c4469
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-16 15:46:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const {body} = require("node-fetch");
 // Add your code here
+function submitData(name, email){
+    const formData = {
+        name,
+        email
+    }
+    return fetch(" http://localhost:3000/users",{
+        method: "POST",
+        headers:{
+            "content-type":"application/json",
+            accept:"application/json",
+        },
+        body:JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(jsonData =>{
+        document.body.innerHTML=jsonData.id;
+    })
+    .catch(message=> document.body.innerHTML=message)
+}
